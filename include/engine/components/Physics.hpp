5161afe88f94299713ecd91ffa54d7f29d93ff58
kereszthury <kereszthurypeter@gmail.com>
2022-07-12 16:06:08 +0200

    Phyisc component update

--- include/engine/components/Physics.hpp
+++ include/engine/components/Physics.hpp
@@ -2,7 +2,7 @@
 
 #include <SFML/System/Vector2.hpp>
 #include "Component.hpp"
-
+#include <algorithm>
 
 namespace engine {
 
@@ -10,12 +10,25 @@ namespace engine {
 
 	class Physics : public Component {
 	public:
+		/*  If a physics object has negative or 0 mass, won't be updated */
+		Physics(float mass = 1.f, float friction = 0.f, float bounciness = 0.f, sf::Vector2f initialVelocity = sf::Vector2f{ 0.f,0.f })
+			: _mass(mass), _friction(std::clamp(friction, 0.f, 1.f)), _bounciness(std::clamp(bounciness, 0.f, 1.f)), _velocity(initialVelocity) {};
+
+		void setMass(float mass);
+		float getMass() const;
+
+		void setFriction(float friction);
+		float getFriction() const;
+
+		void setBounciness(float bounciness);
+		float getBounciness() const;
+
+		void setVelocity(sf::Vector2f velocity);
+		sf::Vector2f getVelocity() const;
 
 		void apply_force(sf::Vector2f force);
 		void setForce(sf::Vector2f force);
-		void setVelocity(sf::Vector2f velocity);
-		void setMass(float mass);
-		void setFriction(float friction);
+		sf::Vector2f getForce() const;
 
 	private:
 		friend Object;
@@ -25,12 +38,13 @@ namespace engine {
 		///////////////
 		// Variables //
 		///////////////
-		sf::Vector2f _sumF;
-		sf::Vector2f _acceleration;
-		sf::Vector2f _velocity;
 		float _mass;
 		float _friction;
 		float _bounciness;	// coefficient of restitution
+
+		sf::Vector2f _velocity;
+		sf::Vector2f _acceleration;
+		sf::Vector2f _sumF;
 
 		void calc_acceleration();
 		void calc_velocity();
