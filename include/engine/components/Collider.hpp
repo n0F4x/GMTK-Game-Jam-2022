c5a12dd5a613f77f46817741d46259a8e504cbf9
kereszthury <kereszthurypeter@gmail.com>
2022-07-12 19:27:06 +0200

    Collider component basics, physics component update

--- include/engine/components/Collider.hpp
+++ include/engine/components/Collider.hpp
@@ -1,10 +1,24 @@
 #pragma once
 
 #include "Component.hpp"
-
+#include "SFML/Graphics/Rect.hpp"
 
 namespace engine {
 
-	class Collider : public Component {};
+	class Collider : public Component {
+	public:
+		// If collisionLayer is -1, collides with nothing, else if is 0 collides with everything, else collides with own layer
+		Collider(sf::FloatRect rect, int layer = 0) : _rectangle(rect), _collisionLayer(layer) {}
+
+		sf::FloatRect getRect() const;
+		void setRect(sf::FloatRect);
+
+		int getLayer() const;
+		int setLayer(int layer);
+
+	private:
+		sf::FloatRect _rectangle;
+		int _collisionLayer;
+	};
 
 }
