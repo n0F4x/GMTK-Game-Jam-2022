01064b98b787541c54a31d699cb9dc5420457db3
Gabor Galgoczy <ggabor2002@gmail.com>
2022-07-15 23:11:04 +0200

    better bezier

--- include/animations/Bezier.hpp
+++ include/animations/Bezier.hpp
@@ -6,15 +6,47 @@
 
 namespace animations {
 
-	class Ease : public engine::Animation {
+	/**
+	 * @brief	Animation class for Cubic-Bezier transitions.
+	 *			Visit https://cubic-bezier.com to visually pick your type of Bezier.
+	*/
+	class Bezier : public engine::Animation {
 	public:
+		Bezier(const sf::Vector2f& point1, const sf::Vector2f& point2);
+
 		void update(sf::Time deltaTime) override;
 		void on_start() override;
 
 	private:
 		sf::Time _timePassed;
 		sf::Vector2f _distanceTraveled;
-		BezierEasing _bezier{ { 0.25f, 0.1f }, { 0.25f, 1.f } };
+		BezierEasing _bezier;
+		const BezierEasing _bezierCopy;
 	};
 
+
+	//////////////////
+	// Most Commons //
+	//////////////////
+
+	class Ease : public Bezier {
+	public:
+		Ease() : Bezier{ { 0.25f, 0.1f }, { 0.25f, 1.f } } {}
+	};
+	class Linear : public Bezier {
+	public:
+		Linear() : Bezier{ { 0.f, 0.f }, { 1.f, 1.f } } {}
+	};
+	class Ease_In : public Bezier {
+	public:
+		Ease_In() : Bezier{ { 0.42f, 0.f }, { 1.f, 1.f } } {}
+	};
+	class Ease_Out: public Bezier {
+	public:
+		Ease_Out() : Bezier{ { 0.f, 0.f }, { 0.58f, 1.f } } {}
+	};
+	class Ease_In_Out : public Bezier {
+	public:
+		Ease_In_Out() : Bezier{ { 0.42f, 0.f }, { 0.58f, 1.f } } {}
+	};
 }
