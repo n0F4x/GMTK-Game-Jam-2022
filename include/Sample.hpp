b93f2343446f2e34c1b881ea5b5d9f87256610f4
kereszthury <kereszthurypeter@gmail.com>
2022-07-16 18:02:01 +0200

    EPIC ANIMATION TIME!!! :O :O :O

--- include/Sample.hpp
+++ include/Sample.hpp
@@ -139,24 +139,50 @@ public:
 		renderer().push_background(&_table);
 
 		_boy.setTexture(&engine::Assets::getTexture("Environment/blue_player"));
-		_boy.setPosition(360, 120);
+		_boy.setPosition(360 - 500, 120 - 500);
 		_boy.scale(8.f, 8.f);
+		addObject(&_boy);
 		renderer().push_background(&_boy);
+		auto animator = _boy.setComponent(std::make_unique<engine::Animator>());
+		animator->addAnimation("in", std::make_unique<animations::Ease>());
+		animator->findAnimation("in")->setDistance({ 500.f, 500.f });
+		animator->findAnimation("in")->setTime(sf::seconds(3));
 
 		_girl.setTexture(&engine::Assets::getTexture("Environment/green_player"));
-		_girl.setPosition(sf::Vector2f(1320, 120));
+		_girl.setPosition(sf::Vector2f(1320 + 500, 120 - 500));
 		_girl.scale(10.f, 10.f);
+		addObject(&_girl);
 		renderer().push_background(&_girl);
+		animator = _girl.setComponent(std::make_unique<engine::Animator>());
+		animator->addAnimation("in", std::make_unique<animations::Ease>());
+		animator->findAnimation("in")->setDistance({ -500.f, 500.f });
+		animator->findAnimation("in")->setTime(sf::seconds(3.5f));
 
 		_grandpa.setTexture(&engine::Assets::getTexture("Environment/red_player"));
-		_grandpa.setPosition(1270, 630);
+		_grandpa.setPosition(1270 + 500, 630 + 500);
 		_grandpa.scale(12.f, 12.f);
+		addObject(&_grandpa);
 		renderer().push_background(&_grandpa);
+		animator = _grandpa.setComponent(std::make_unique<engine::Animator>());
+		animator->addAnimation("in", std::make_unique<animations::Ease>());
+		animator->findAnimation("in")->setDistance({ -500.f, -500.f });
+		animator->findAnimation("in")->setTime(sf::seconds(4));
 
 		_dog.setTexture(&engine::Assets::getTexture("Environment/yellow_player"));
-		_dog.setPosition(sf::Vector2f(350, 700));
+		_dog.setPosition(sf::Vector2f(350 - 500, 700 + 500));
 		_dog.scale(8.f, 8.f);
+		addObject(&_dog);
 		renderer().push_background(&_dog);
+		animator = _dog.setComponent(std::make_unique<engine::Animator>());
+		animator->addAnimation("in", std::make_unique<animations::Ease>());
+		animator->findAnimation("in")->setDistance({ 500.f, -500.f });
+		animator->findAnimation("in")->setTime(sf::seconds(4.5f));
+
+		//TODO - remove
+		_boy.getComponent<engine::Animator>()->findAnimation("in")->start();
+		_girl.getComponent<engine::Animator>()->findAnimation("in")->start();
+		_grandpa.getComponent<engine::Animator>()->findAnimation("in")->start();
+		_dog.getComponent<engine::Animator>()->findAnimation("in")->start();
 	}
 
 	void handle_event(const sf::Event& event) override {
