1f5dcb3b20c000aefc197083060041b9606dd2d8
Ármin <arminszilczer@gamil.com>
2022-07-16 18:30:07 +0200

    Character calculations

--- src/states/Character.cpp
+++ src/states/Character.cpp
@@ -6,16 +6,20 @@ void Character::setHappiness(const int amount) {
     _happiness = amount;
     if (_happiness > MAX_HAPPINESS)
         _happiness = MAX_HAPPINESS;
-    else if (_happiness < 0)
+    else if (_happiness < 0){
         _happiness = 0;
+        _gameOverCallback();
+    }
 }
 
 void Character::addHappiness(const int amount) {
     _happiness += amount;
     if (_happiness > MAX_HAPPINESS)
         _happiness = MAX_HAPPINESS;
-    else if (_happiness < 0)
+    else if (_happiness < 0){
         _happiness = 0;
+        _gameOverCallback();
+    }
 }
 
 int Character::getHappiness() const {
@@ -24,4 +28,21 @@ int Character::getHappiness() const {
 
 float Character::getHappinessNormed() const {
     return (float)_happiness / (float)MAX_HAPPINESS;
+}
+
+void Character::calculateHappinessAfterOwnTurn(int diceNumber, TileType tile) {
+    if (diceNumber == _favoriteNumber)
+        addHappiness(_favoriteNumberHappinessChange);
+    else if (diceNumber == _hatedNumber)
+        addHappiness(_hatedNumberHappinessChange);
+    if (tile == _favoriteTile)
+        addHappiness(_favoriteTileHappinessChange);
+    else if (tile == _hatedTile)
+        addHappiness(_hatedTileHappinessChange);
+
+    addHappiness(_specialCallback());
+}
+
+void Character::calculateHappinessAfterOthersTurn() {
+    addHappiness(-_happinessLoss);
 }
