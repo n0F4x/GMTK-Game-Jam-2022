d4521f6b74572a4ac719d2d1454cb7b0f8fda831
Gabor Galgoczy <ggabor@gmail.com>
2022-07-16 21:42:15 +0200

    theme

--- include/states/description/DescriptionState.hpp
+++ include/states/description/DescriptionState.hpp
@@ -1,13 +1,9 @@
 #pragma once
 
 #include "engine/State.hpp"
-#include "engine/StateMachine.hpp"
+
 
 class DescriptionState : public engine::State {
-public:
-    DescriptionState();
-
 private:
     void on_draw() override;
-
 };
