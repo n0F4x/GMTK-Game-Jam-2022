864a5b3bb53b4c1b87d0204b34efd7a2ae1295ec
Gabor Toth <gabor6505@gmail.com>
2022-07-16 17:08:34 +0200

    Started working on Tiles and TileManager

---
+++ include/game/TileManager.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <memory>
+#include "engine/drawables/Sprite.hpp"
+#include "game/TileType.hpp"
+
+using namespace engine;
+
+class TileManager : Sprite {
+public:
+    TileManager() {
+        attach_child(firstTile.get());
+        firstTile->setPosition(getPosition());
+    }
+
+    void addTile(TileType type, TileDirection dir) {
+        lastTile->next = std::make_unique<Tile>(type);
+        lastTile->dirToNext = dir;
+        lastTile->next->dirFromPrev = dir;
+
+        switch (dir) {
+            case LEFT:
+                lastTile->next->setPosition({lastTile->getPosition().x - lastTile->getSize().x, lastTile->getPosition().y});
+                break;
+            case RIGHT:
+                lastTile->next->setPosition({lastTile->getPosition().x + lastTile->getSize().x, lastTile->getPosition().y});
+                break;
+            case UP:
+                lastTile->next->setPosition({lastTile->getPosition().x, lastTile->getPosition().y - lastTile->getSize().y});
+                break;
+            case DOWN:
+                lastTile->next->setPosition({lastTile->getPosition().x, lastTile->getPosition().y + lastTile->getSize().y});
+                break;
+            case NONE:
+                break;
+        }
+
+        lastTile = lastTile->next.get();
+        attach_child(lastTile);
+    }
+private:
+    std::unique_ptr<Tile> firstTile = std::make_unique<Tile>(START);
+    Tile* lastTile = firstTile.get();
+};
