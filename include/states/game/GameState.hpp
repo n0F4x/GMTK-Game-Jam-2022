4500ae4f9e2132cecde462e73ef99d70a58b3286
Gabor Galgoczy <ggabor@gmail.com>
2022-07-17 13:01:18 +0200

    Removed GameInitState

--- include/states/game/GameState.hpp
+++ include/states/game/GameState.hpp
@@ -2,6 +2,8 @@
 
 #include "engine/State.hpp"
 #include "engine/StateMachine.hpp"
+#include "engine/drawables/Sprite.hpp"
+#include "UI/ProgressBar.hpp"
 
 
 class GameState : public engine::State {
@@ -17,8 +19,30 @@ protected:
 
 
 private:
+	void background_setup();
+	void character_setup();
+
 	///////////////
 	// Variables //
 	///////////////
 	engine::StateMachine _stateMachine;
+
+
+	// Background
+	engine::Sprite _floor;
+	engine::Sprite _table;
+	engine::Sprite _table2;
+	engine::Sprite _title;
+	engine::Sprite _croissant;
+	engine::Sprite _plant;
+	engine::Sprite _plant2;
+	engine::Sprite _coffee;
+	engine::Object _scaler;
+
+
+	bool _characterAnimations = false;
+	// Characters
+	engine::Sprite _boy, _girl, _grandpa, _dog;
+	engine::Sprite _boyBarBack, _girlBarBack, _gradpaBarBack, _dogBarBack;
+	UI::ProgressBar _boyBar{ {392.f, 32.f} }, _girlBar{ {392.f, 32.f} }, _grandpaBar{ {392.f, 32.f} }, _dogBar{ {392.f, 32.f} };
 };
